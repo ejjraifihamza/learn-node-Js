ef8929a6d69172369e2838484334fd90dccc0a68
Ejjraifi Hamza <ejjraifihamza@gmail.com>
2021-12-20 15:01:25 +0100

    🎉 Node.js File Server

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 /*
-!The Built-in URL Module
-The URL module splits up a web address into readable parts.
-
-To include the URL module, use the require() method:
-let url = require('url')
+!Node.js File Server
+Now we know how to parse the query string, and in the file server chapter we learned how to make Node.js behave as a file server. 
+Let us combine the two, and serve the file requested by the client.
 */
-// ?Parse an address with the url.parse() method, and it will return a URL object with each part of the address as properties:
-// *Example (Split a web address into readable parts)
+// ?Create a Node.js file that opens the requested file and returns the content to the client. If anything goes wrong, throw a 404 error:
+let http = require("http");
+let fs = require("fs");
 let url = require("url");
-let adr = "http://localhost:8080/default.htm?year=2017&month=february";
-let q = url.parse(adr, true);
-console.log(q.host); // returns 'localhost:8080'
-console.log(q.pathname); // returns '/default.htm'
-console.log(q.search); // returns '?year=2017&month=february'
-let qdata = q.query; // returns an object: { year: 2017, month: 'february' }
-console.log(qdata.year); // returns '2017'
-console.log(qdata.month); // returns 'february'
+http
+  .createServer((req, res) => {
+    let q = url.parse(req.url, true);
+    let filename = `.${q.pathname}`;
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-type": "text/html" });
+        return res.end("404 Not Found");
+      }
+      res.writeHead(200, { "Content-type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
