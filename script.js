0f1d2399d8b96349e1e6698db6e843eb8ceb36e2
Ejjraifi Hamza <ejjraifihamza@gmail.com>
2021-12-20 14:26:40 +0100

    🎉  Node.js URL Module

--- script.js
+++ script.js
@@ -1,94 +1,18 @@
 /*
-?Upload files
-The Formidable Module
-*There is a very good module for working with file uploads, called "Formidable".
--The Formidable module can be downloaded and installed using NPM: npm install formidable
--After you have downloaded the Formidable module, you can include the module in any application: let formidable = require('formidable')
+!The Built-in URL Module
+The URL module splits up a web address into readable parts.
+
+To include the URL module, use the require() method:
+let url = require('url')
 */
-
-// Now you are ready to make a web page in Node.js that lets the user upload files to your computer:
-// !Step 1: Create an Upload Form
-// Create a Node.js file that writes an HTML form, with an upload field:
-// ?Example (This code will produce an HTML form:)
-
-// let http = require("http");
-// http
-//   .createServer((req, res) => {
-//     res.writeHead(200, { "Content-type": "text/html" });
-//     res.write(
-//       '<form action="fileupload" method="post" enctype="multipart/form-data">'
-//     );
-//     res.write('<input type="file" name="filetoupload"><br>');
-//     res.write('<input type="submit">');
-//     res.write("</form>");
-//     return res.end();
-//   })
-//   .listen(8080);
-
-// !Step 2: Parse the Uploaded File
-// Include the Formidable module to be able to parse the uploaded file once it reaches the server.
-// When the file is uploaded and parsed, it gets placed on a temporary folder on your computer.
-// ?Example (The file will be uploaded, and placed on a temporary folder:)
-
-// let http = require("http");
-// let formidable = require("formidable");
-
-// http
-//   .createServer((req, res) => {
-//     if (req.url == "/fileuploade") {
-//       let form = new formidable.IncomingForm();
-//       form.parse(req, (err, fields, fiels) => {
-//         res.write("File uploaded");
-//         res.end();
-//       });
-//     } else {
-//       res.writeHead(200, { "Content-Type": "text/html" });
-//       res.write(
-//         '<form action="fileupload" method="post" enctype="multipart/form-data">'
-//       );
-//       res.write('<input type="file" name="filetoupload"><br>');
-//       res.write('<input type="submit">');
-//       res.write("</form>");
-//       return res.end();
-//     }
-//   })
-//   .listen(8080);
-
-/*
-!Step 3: Save the File
-When a file is successfully uploaded to the server, it is placed on a temporary folder.
-
-The path to this directory can be found in the "files" object, passed as the third argument in the parse() method's callback function.
-
-To move the file to the folder of your choice, use the File System module, and rename the file:
-*/
-
-let http = require("http");
-let formidable = require("formidable");
-let fs = require("fs");
-
-http
-  .createServer((req, res) => {
-    if (req.url == "/fileupload") {
-      let form = new formidable.IncomingForm();
-      form.parse(req, (err, fields, files) => {
-        let oldPath = files.filetoupload.filepath;
-        let newPath = `C:/Users/Youcode/${files.filetoupload.originalFilename}`;
-        fs.rename(oldPath, newPath, (err) => {
-          if (err) throw err;
-          res.write("File uploaded and moved!");
-          res.end();
-        });
-      });
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(
-        '<form action="fileupload" method="post" enctype="multipart/form-data">'
-      );
-      res.write('<input type="file" name="filetoupload"><br>');
-      res.write('<input type="submit">');
-      res.write("</form>");
-      return res.end();
-    }
-  })
-  .listen(8080);
+// ?Parse an address with the url.parse() method, and it will return a URL object with each part of the address as properties:
+// *Example (Split a web address into readable parts)
+let url = require("url");
+let adr = "http://localhost:8080/default.htm?year=2017&month=february";
+let q = url.parse(adr, true);
+console.log(q.host); // returns 'localhost:8080'
+console.log(q.pathname); // returns '/default.htm'
+console.log(q.search); // returns '?year=2017&month=february'
+let qdata = q.query; // returns an object: { year: 2017, month: 'february' }
+console.log(qdata.year); // returns '2017'
+console.log(qdata.month); // returns 'february'
