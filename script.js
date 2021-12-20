8ddae588d3a7050d61e81eb66c2800499bcb173b
Ejjraifi Hamza <ejjraifihamza@gmail.com>
2021-12-20 16:57:01 +0100

    🎉 NodeJs Npm

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 /*
-!Node.js File Server
-Now we know how to parse the query string, and in the file server chapter we learned how to make Node.js behave as a file server. 
-Let us combine the two, and serve the file requested by the client.
+!What is NPM?
+*NPM is a package manager for Node.js packages, or modules if you like.
+?The NPM program is installed on your computer when you install Node.js
+What is a Package?
+A package in Node.js contains all the files you need for a module.
+
+Modules are JavaScript libraries you can include in your project.
+!Download a Package
+Downloading a package is very easy.
+
+Open the command line interface and tell NPM to download the package you want.
+
+I want to download a package called "upper-case":
+npm install upper-case
+Now you have downloaded and installed your first package!
+NPM creates a folder named "node_modules", where the package will be placed. All packages you install in the future will be placed in this folder.
+!Using a Package
+Once the package is installed, it is ready to use.
+
+Include the "upper-case" package the same way you include any other module:
+let uc = require('upper-case')
+Create a Node.js file that will convert the output "Hello World!" into upper-case letters:
 */
-// ?Create a Node.js file that opens the requested file and returns the content to the client. If anything goes wrong, throw a 404 error:
+// ?Example
 let http = require("http");
-let fs = require("fs");
-let url = require("url");
+let uc = require("upper-case");
 http
   .createServer((req, res) => {
-    let q = url.parse(req.url, true);
-    let filename = `.${q.pathname}`;
-    fs.readFile(filename, (err, data) => {
-      if (err) {
-        res.writeHead(404, { "Content-type": "text/html" });
-        return res.end("404 Not Found");
-      }
-      res.writeHead(200, { "Content-type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write(uc.upperCase("Hello World!"));
+    res.end();
   })
   .listen(8080);
